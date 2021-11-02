8b376a2fffc50df512fdd5b35fbcb89b76ede44a
Shruti <shrutiverma28022802@gmail.com>
2021-11-03 04:48:33 +0530

    docenv

--- index.js
+++ index.js
@@ -17,60 +17,60 @@ app.get('/', (req, res) =>{
     res.send('Hello World!!');
 })
 
-// app.get('/quiz', (req, res) => {
-//     // res.send()
-//     res.redirect('/quiz/game');
-// })
-// app.post('/quiz/game/:id/return', (req, res) => {
+app.get('/quiz', (req, res) => {
+    // res.send()
+    res.redirect('/quiz/game');
+})
+app.post('/quiz/game/:id/return', (req, res) => {
     
-//     pool.connect(function(err) {
-//         let score = req.body.score;
-//         if (err) throw err;
-//         var id = req.params.id;
-//         pool.connect(function(err) {
-//             if (err) throw err;
-//             var sql = "UPDATE users SET score = ? WHERE user_id = ?"+ (score, id);
-//             pool.query(sql, function (err, result) {
-//               if (err) throw err;
-//               console.log(result.affectedRows + " record(s) updated");
-//             });
-//           });
-//     })
-// // })
+    pool.connect(function(err) {
+        let score = req.body.score;
+        if (err) throw err;
+        var id = req.params.id;
+        pool.connect(function(err) {
+            if (err) throw err;
+            var sql = "UPDATE users SET score = ? WHERE user_id = ?"+ (score, id);
+            pool.query(sql, function (err, result) {
+              if (err) throw err;
+              console.log(result.affectedRows + " record(s) updated");
+            });
+          });
+    })
+})
 
-// // app.get('/leaderboard', (req, res) => {
+app.get('/leaderboard', (req, res) => {
      
-// //     pool.query("SELECT * FROM users", function (err, result, fields) {
-// //         if (err) throw err;
-// //         res.send(result);
-// //       })
-// // })
+    pool.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      })
+})
 
-// // app.get('/quiz/game/return', (req, res) => {
-// //     pool.query("SELECT score FROM users", function (err, result, fields) {
-// //         if (err) throw err;
-// //         res.send(score);
-// //       })
-// // })
+app.get('/quiz/game/return', (req, res) => {
+    pool.query("SELECT score FROM users", function (err, result, fields) {
+        if (err) throw err;
+        res.send(score);
+      })
+})
 
-// app.get('/quiz/game', (req, res) => {
+app.get('/quiz/game', (req, res) => {
 
-//     let quiz = [];
+    let quiz = [];
 
-//     pool.query("SELECT * FROM Questions", function (err, result, fields) {
-//         if (err) throw err;
-//         // console.log(result);
-//         while(quiz.length<5){
-//             var r = Math.floor(Math.random() * result.length);
-//             if(quiz.indexOf(r) === -1) quiz.push(result[r])
-//         }
-//         console.log("quiz is here", quiz);
-//         res.send(quiz);
-//       })
+    pool.query("SELECT * FROM Questions", function (err, result, fields) {
+        if (err) throw err;
+        // console.log(result);
+        while(quiz.length<5){
+            var r = Math.floor(Math.random() * result.length);
+            if(quiz.indexOf(r) === -1) quiz.push(result[r])
+        }
+        console.log("quiz is here", quiz);
+        res.send(quiz);
+      })
     
-//  })
+ })
 
-//app.use('/api', routes); 
+app.use('/api', routes); 
 
 server.listen(PORT,  function() {
     console.log("here is the port");
