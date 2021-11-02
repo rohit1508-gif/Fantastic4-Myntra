85b0dde71af41f6b32666370268bdb51b3046f25
Shruti <shrutiverma28022802@gmail.com>
2021-11-02 19:38:11 +0530

    i

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
-const { createPool } = require('mysql');
+// const { createPool } = require('mysql');
 
-const pool = createPool({
-    port: '3306',
-    host: 'beqdfetas6as1orjunp2-mysql.services.clever-cloud.com',
-    user: 'uktonoxzm21uepge',
-    password: 'K2rkHRTBkmWiAh72Kkiv',
-    database: 'beqdfetas6as1orjunp2',
-    connectionLimit: '10',
-});
+// const pool = createPool({
+//     port: '3306',
+//     host: 'beqdfetas6as1orjunp2-mysql.services.clever-cloud.com',
+//     user: 'uktonoxzm21uepge',
+//     password: 'K2rkHRTBkmWiAh72Kkiv',
+//     database: 'beqdfetas6as1orjunp2',
+//     connectionLimit: '10',
+// });
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        throw err;
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         throw err;
     
-    }
-    else {
-        console.log('MySQL connected...');
-    }
-    connection.release();
-    return;
-});
+//     }
+//     else {
+//         console.log('MySQL connected...');
+//     }
+//     connection.release();
+//     return;
+// });
 
-module.exports = pool;
+// module.exports = pool;
