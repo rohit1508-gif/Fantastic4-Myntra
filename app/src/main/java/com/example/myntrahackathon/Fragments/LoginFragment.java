95781f65a368f6ad6f703b513ce5fb8f53ba3033
Abhinav Rishi <abhinavrishi@myrocket.in>
2021-11-04 08:57:50 +0530

    Log In Animation

--- app/src/main/java/com/example/myntrahackathon/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/myntrahackathon/Fragments/LoginFragment.java
@@ -1,10 +1,12 @@
 package com.example.myntrahackathon.Fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.myntrahackathon.R;
 
 public class LoginFragment extends Fragment {
@@ -28,24 +31,32 @@ public class LoginFragment extends Fragment {
 
         EditText etEmail = view.findViewById(R.id.etEmail);
         EditText etPassword = view.findViewById(R.id.etPassword);
+        ProgressBar progressBar = view.findViewById(R.id.progressBarLogin);
 
-        view.findViewById(R.id.btnLogIn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (authenticate(etEmail.getText().toString().trim(), etPassword.getText().toString().trim())) {
+        view.findViewById(R.id.btnLogIn).setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            if (authenticate(etEmail.getText().toString().trim(), etPassword.getText().toString().trim())) {
+                progressBar.setVisibility(View.GONE);
+                LottieAnimationView lottieAnimationView = view.findViewById(R.id.lottie_login_successful);
+                lottieAnimationView.setVisibility(View.VISIBLE);
+                lottieAnimationView.playAnimation();
+
+                new Handler().postDelayed(() -> {
                     if (getFragmentManager() != null) {
                         FragmentTransaction transaction = getFragmentManager().beginTransaction();
                         transaction.replace(R.id.fragmentContainer, new HomeFragment());
                         transaction.commit();
                     }
-                } else {
-                    Toast.makeText(getContext(), "Wrong Email ID or Password !!", Toast.LENGTH_SHORT).show();
-                }
+                }, 1000);
+
+            } else {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getContext(), "Wrong Email ID or Password !!", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private Boolean authenticate(String emailId, String password) {
-        return false;
+        return true;
     }
 }
