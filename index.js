d38da3410f2b5efc6a9e042eb21f88c7a7794c47
Shruti <shrutiverma28022802@gmail.com>
2021-11-06 06:24:54 +0530

    login and question2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
-// const pool = require('./config/database');
-
+const pool = require('./config/database');
+var flash             = require('connect-flash');
+var crypto            = require('crypto');
+var passport          = require('passport');
+var LocalStrategy     = require('passport-local').Strategy;
+var sess              = require('express-session');
+var Store             = require('express-session').Store;
+var BetterMemoryStore = require(__dirname + '/memory');
 
 const app = express();
 //const routes = require('./router');
@@ -10,17 +16,120 @@ const app = express();
 const PORT = process.env.PORT || 3300;
 const server = http.createServer(app);
 
-app.use(express.json());
 
-app.get('/', (req, res) =>{
-    
+var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
+
+app.use(sess({
+
+   name: 'JSESSION',
+
+   secret: 'MYSECRETISVERYSECRET',
+
+   store:  store,
+
+   resave: true,
+
+   saveUninitialized: true
+
+}));
+
+app.use(flash());
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+passport.use('local', new LocalStrategy({
+
+    usernameField: 'username',
+  
+    passwordField: 'password',
+  
+    passReqToCallback: true //passback entire req to call back
+  } , function (req, username, password, done){
+  
+  
+        if(!username || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
+  
+        var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
+        pool.connect(function(err) {
+            if (err) throw err;
+        pool.query("select * from users where username = ?", [username], function(err, rows){
+  
+            console.log(err); console.log(rows);
+  
+          if (err) return done(req.flash('message',err));
+  
+          if(!rows.length){ return done(null, false, req.flash('message','Invalid username or password.')); }
+  
+          salt = salt+''+password;
+  
+          var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
+  
+  
+          var dbPassword  = rows[0].password;
+  
+          if(!(dbPassword == encPassword)){
+  
+              return done(null, false, req.flash('message','Invalid username or password.'));
+  
+           }
+  
+          return done(null, rows[0]);
+  
+        });
+    })
+      }
+  
+  ));
+
+  passport.serializeUser(function(user, done){
+
+    done(null, user.id);
+
+});
+
+passport.deserializeUser(function(id, done){
+    pool.connect(function(err) {
+        if (err) throw err;
+    pool.query("select * from tbl_users where id = "+ id, function (err, rows){
+
+        done(err, rows[0]);
+
+    });
+  })
+});
+
+app.get('/', isAuthenticated, (req, res) =>{
     res.send('Hello World!!');
 })
 
-app.get('/quiz', (req, res) => {
-    // res.send()
+app.get('/signin', function(req, res){
+    res.render('logged in');
+  });
+
+app.post("/signin", passport.authenticate('local', {
+
+    successRedirect: '/',
+
+    failureRedirect: '/signin',
+
+    failureFlash: true
+
+}), function(req, res, info){
+
+    res.render('login');
+
+});
+
+app.get('/quiz',isAuthenticated, (req, res) => {
+    pool.query("SELECT * FROM Quiz", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      })
     res.redirect('/quiz/game');
 })
+
 app.post('/quiz/game/:id/return', (req, res) => {
     
     pool.connect(function(err) {
@@ -38,7 +147,7 @@ app.post('/quiz/game/:id/return', (req, res) => {
     })
 })
 
-app.get('/leaderboard', (req, res) => {
+app.get('/leaderboard',isAuthenticated, (req, res) => {
      
     pool.query("SELECT * FROM users", function (err, result, fields) {
         if (err) throw err;
@@ -53,27 +162,55 @@ app.get('/quiz/game/return', (req, res) => {
       })
 })
 
-app.get('/quiz/game', (req, res) => {
+app.get('/quiz/:id/game',isAuthenticated, (req, res) => {
 
     let quiz = [];
-
+    let id = req.params.id;
+    if(id==1){
     pool.query("SELECT * FROM Questions", function (err, result, fields) {
         if (err) throw err;
         // console.log(result);
         while(quiz.length<5){
             var r = Math.floor(Math.random() * result.length);
             if(quiz.indexOf(r) === -1) quiz.push(result[r])
+            console.log(r);
         }
         console.log("quiz is here", quiz);
         res.send(quiz);
       })
+    }
+    else if(id==2){
+        pool.query("SELECT * FROM Questions_2", function (err, result, fields) {
+            if (err) throw err;
+            // console.log(result);
+            while(quiz.length<5){
+                var r = Math.floor(Math.random() * result.length);
+                if(quiz.indexOf(r) === -1) quiz.push(result[r])
+                console.log(r);
+            }
+            console.log("quiz is here", quiz);
+            res.send(quiz);
+          })
+    }
     
- })
+})
 
-app.use('/api', routes); 
+function isAuthenticated(req, res, next) {
 
-server.listen(PORT,  function() {
-    console.log("here is the port");
-    console.log("portttttttt"+ PORT);
-    console.log("Listening on Port 3300");
+    if (req.isAuthenticated())
+  
+      return next();
+  
+    res.redirect('/signin');
+  
+  }
+
+
+
+app.use(express.json());
+//app.use('/api', routes); 
+
+server.listen(PORT, () => {
+    console.log('Server up and running on ', PORT);
+
 });
